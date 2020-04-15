d726fc3207ca18cf0245eae859243632082ad9f9
asengelhart <asengelhart@yahoo.com>
2020-04-15 15:34:43 -0600

    adding templates to models

--- frontend/src/models.js
+++ frontend/src/models.js
@@ -1,35 +1,97 @@
 class Comment {
-  constructor(id, content, ticket_id, user_id) {
-    this._id = id;
-    this._content = content;
-    this._ticket_id = ticket_id;
-    this._user_id = user_id;
+  constructor(options) {
+    this._id = options.id;
+    this._content = options.content;
+    this._ticket_id = options.ticket_id;
+    this._user = options.user;
   }
 
-  get id() {
-    return this._id;
-  }
-
-  get content() {
-    return this._content;
-  }
-
-  get ticket_id() {
-    return this._ticket_id;
-  }
-
-  get user_id() {
-    return this._user_id;
+  render() { 
+    return `
+      <div class="row">
+        <div class="col s8 blue">
+          <p>${this._user.username} writes:</p>
+          <p>${this._content}</p>
+        </div>
+      </div>
+    `
   }
 }
 
 class Ticket {
-  constructor(id, subject, content, user_id, all_comments = null) {
-    this._id = id;
-    this._subject = subject;
-    this._content = content;
-    this._user_id = user_id;
-    this._all_comments = all_comments;
+  constructor(options) {
+    this._id = options.id;
+    this._subject = options.subject;
+    this._content = options.content;
+    this._user = options.user;
+    this._urgency = options.urgency;
+    this._comments = options.comments.map(commentObject => new Comment(commentObject));
+  }
+
+  render() {
+    return `
+    <ul class="collapsible">
+    <li>
+      <div class="collapsible-header ticket-header">
+        <div class="row ticket-row">${this._subject}</div>
+        <div class="row ticket-row">Urgency: ${this._urgency}</div>
+      </div>
+      <div class="collapsible-body">
+        <div class="ticket-content"><p>${this._content}</p></div>
+        ${this.renderAllComments()}
+      </div>
+    </li>
+  </ul>
+  `
+  }
+
+  
+
+  formTemplate() {
+    return `
+    <div class="row">
+      <form id="create_ticket_form" class="col s12">
+        <div class="row">
+          <div class="input-field col s8">
+            <label for="ticket_subject">Subject:</label>
+            <input placeholder="What issue are you having?" type="text" name="ticket_subject" id="ticket_subject" class="validate">
+          </div>
+        </div>
+        <p>How urgent is your issue?</p>
+        <div class="row">
+          <div class="col s4">
+            <label>
+              <input type="radio" name="urgency" id="urgency_0" value="0">
+              <span>It's an annoyance.</span>
+            </label>
+          </div>
+          <div class="col s4">
+            <label>
+              <input type="radio" name="urgency" id="urgency_1" value="1">
+              <span>It's hurting my productivity.</span>
+            </label>
+          </div>
+          <div class="col s4">
+            <label>
+              <input type="radio" name="urgency" id="urgency_2" value="2">
+              <span>I can't work because of this.</span>
+            </label>
+          </div>
+        </div>
+        <button type="submit" class="btn">Create Ticket</button>
+      </form>
+    </div>
+    `
+  }
+
+
+
+  renderAllComments() {
+    let result = ``
+    for(const comment of this._comments) {
+      result += `${comment.render()}`
+    }
+    return result;
   }
 }
 
