3639d745662eafaba3aa9b9b390603a2d7099724
asengelhart <asengelhart@yahoo.com>
2020-04-19 10:17:04 -0600

    added sessions

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,2 +1,32 @@
 class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:id] = @user.id
+      render json: render_current_user
+    else
+      render json: {message: "Login failed"}, status: :unauthorized
+    end
+  end
+
+  def show
+    render json: render_current_user
+  end
+
+  private
+
+  def render_current_user
+    if current_user
+      {
+        user: current_user.to_json(only: [:id, :username, :email, :is_admin]),
+        logged_in: true
+      }
+    else
+      {
+        user: nil,
+        logged_in: false
+      }
+    end
+  end
+
 end
