d29346a19fb76e6642dfee0d68858c9bdd26ba6f
asengelhart <asengelhart@yahoo.com>
2020-04-19 15:04:31 -0600

    routing and cors fixes to enable login

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get "/tickets", to: "tickets#index"
   get "/tickets/:id", to: "tickets#show"
   post "/tickets", to: "tickets#create"
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
+  get "/current_user", to: "sessions#show"
+  post "/comments", to: "comments#create"
 end
