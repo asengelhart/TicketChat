4515c417c27af2ade8522590b628c777ef58dd83
asengelhart <asengelhart@yahoo.com>
2020-04-19 15:04:43 -0600

    logout functionality

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_by(params[:email])
-    if @user && @user.authenticate(params[:password])
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
       session[:id] = @user.id
       render json: render_current_user
     else
@@ -13,12 +13,17 @@ class SessionsController < ApplicationController
     render json: render_current_user
   end
 
+  def destroy
+    session[:id] = nil
+    render json: render_current_user
+  end
+
   private
 
   def render_current_user
     if current_user
       {
-        user: current_user.to_json(only: [:id, :username, :email, :is_admin]),
+        user: current_user.as_json(only: [:id, :username, :email, :is_admin]),
         logged_in: true
       }
     else
